6ecb4fb00354ef3d3967dae574106d9f1bd34508
oysy <479811282@qq.com>
2023-05-28 06:06:24 +0800

    train

--- train.py
+++ train.py
@@ -59,47 +59,10 @@ def get_transform(args):
                   ]
 
     return T.Compose(transforms)
-
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
-
-    def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
-
-        logpt = F.log_softmax(input, dim=-1)
-        logpt = logpt.gather(1,target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
-
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            # print(logpt, at, logpt.shape, at.shape)
-            logpt = logpt * Variable(at)
-
-        loss = -1 * (1-pt)**self.gamma * logpt
-        if self.size_average: return loss.mean()
-        else: return loss.sum()
+    
 
 def criterion(input, target):
     weight = torch.FloatTensor([0.9, 1.1]).cuda()
-    # FL = FocalLoss(gamma=2, alpha=0.55)
-    # l1 = FL(input, target)
-    # l2 = nn.functional.cross_entropy(input, target, weight=weight)
-    # print(l1, l2)
-    # return l1*10
-    # return (l1*10 + l2)/2
     return nn.functional.cross_entropy(input, target, weight=weight)
 
 
